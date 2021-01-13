096343b41842635b9644c48d6bd6f919bf00b0b8
Bartosz Rogala <Bartosz.j.rogala@gmail.com>
2021-01-13 19:06:13 +0100

    Update learnmore/README

--- Character-Creator-frontend/client/src/components/LearnMoreComponent.js
+++ Character-Creator-frontend/client/src/components/LearnMoreComponent.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class LearnMoreComponent extends Component {
     render() {
         return (
             <div>
-                Lorem Ipsum
+                <Link to="/">
+                        <button className="ui secondary button">
+                            Back
+                        </button>
+                </Link>
+                
+                <div className="ui icon compact message">
+                    <i className="medapps icon"></i>
+                    <div className="aligned center ui">
+                        <b>DnD character creator</b> is a hobby project created by <b>Bartosz Rogala</b>. 
+                        <br></br>
+                        <br></br>
+                        DnD character creator allows user to quickly and easily create DnD character and generate character sheet. It also allows user to view all possible stats that character can have
+                        <br></br>
+                        <br></br>
+                        It is still in development. <br></br>
+                        if you have any suggestions or questions, please contact me: bartosz.j.rogala@gmail.com
+                    </div>
+                </div>
+
+                <br></br>
+                <br></br>
+
+                <h2> DnD character creator is a fullstack application: </h2>
+
+                <h3>Fontend-side tools and technologies used: </h3>
+                <div className="ui middle aligned bulleted animated list">
+                    <div className="item"> React (JavaScript library) </div>
+                    <div className="item"> Modern JavaScript (ES6) </div>
+                    <div className="item"> Node JS and NPM (to install and create the project) </div>
+                    <div className="item"> Create React App CLI </div>
+                    <div className="item"> Semantic UI (front-end development framework) </div>
+                    <div className="item"> Axios HTTP Library </div>
+                </div>
+
+                <h3> Server-side technologies used: </h3>
+                    <div className="ui middle aligned bulleted animated list">
+                    <div className="item"> Spring Boot </div>
+                    <div className="item"> SpringData JPA (Hibernate) </div>
+                    <div className="item"> Maven </div>
+                    <div className="item"> MySQL Database </div>
+                </div>
+
+
+                <br></br>
+
+                <div className="ui icon negative compact message">
+                    <i className="notched circle loading icon"></i>
+                    <div className="content">
+                        <div className="header">
+                            To be developed:
+                        </div>
+                        <ul className="list">
+                            <li> finish character creation </li>
+                            <li> user account (Spring Security) </li>
+                            <li> view already created characters </li>
+                            <li> upgrade created characters </li>
+                        </ul>
+                    </div>
+                </div>
+
+                
             </div>
         );
     }
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Springboot + React App for creating Dungeons and Dragons character sheet
+DnD character creator is a hobby project created by Bartosz Rogala. It is still in development.
+if you have any suggestions or questions, please contact me: bartosz.j.rogala@gmail.com
+
+DnD character creator is a fullstack application:
+
+Fontend-side tools and technologies used:
+- React (JavaScript library)
+- Modern JavaScript (ES6)
+- Node JS and NPM (to install and create the project)
+- Create React App CLI
+- Semantic UI (front-end development framework)
+- Axios HTTP Library
+
+Server-side technologies used:
+- Spring Boot
+- SpringData JPA (Hibernate)
+- Maven
+- MySQL Database
+
+
+DnD character creator allows user to quickly and easily create DnD character and generate character sheet. It also allows user to view all possible stats that character can have
+
+
+To be developed:
+- finish character creation
+- user account
+- view already created characters
+- upgrade created characters
